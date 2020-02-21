94e6a395f6b4ceefa115538583c06c9b029e3230
Cesar Marin <cesar.marin@wolterskluwer.com>
2020-02-21 10:12:58 +0100

    configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -19,13 +19,15 @@ describe('AppComponent', () => {
   it(`should have as title 'wk-web-components'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('wk-web-components');
+    expect(app.title).toEqual("wk-web-components");
   });
 
-  it('should render title in a h1 tag', () => {
+  it("should render title in a h1 tag", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wk-web-components!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to wk-web-components!"
+    );
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,40 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+
+declare global {
+  interface Window {
+    WebComponents: {
+      ready: boolean;
+    };
+  }
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+function bootstrapModule() {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .then(ref => {
+      // Ensure Angular destroys itself on hot reloads.
+      if (window["ngRef"]) {
+        window["ngRef"].destroy();
+      }
+      window["ngRef"] = ref;
+
+      // Otherwise, log the boot error
+    })
+    .catch(err => console.error(err));
+}
+
+if (window.WebComponents.ready) {
+  // Web Components are ready
+  bootstrapModule();
+} else {
+  // Wait for polyfills to load
+  window.addEventListener("WebComponentsReady", bootstrapModule);
+}
